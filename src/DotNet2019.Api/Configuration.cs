4e13a2d12facc5f437facbccf1c7a1d184773d71
Carlos Landeras <clanderas@plainconcepts.com>
2019-06-16 12:16:15 +0200

    Add build and k8s solution folders for better browsing in talk

--- src/DotNet2019.Api/Configuration.cs
+++ src/DotNet2019.Api/Configuration.cs
@@ -2,6 +2,7 @@ using DotNet2019.Api.Infrastructure.Middleware;
 using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
@@ -37,6 +38,11 @@ namespace DotNet2019.Api
                             name: "default",
                             pattern: "{controller=Home}/{action=Index}/{id?}");
                     endpoints.MapRazorPages();
+
+                    endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync($"Hello DotNet 2019! from {System.Environment.MachineName}");
+                    });
 
                     endpoints.MapSecretEndpoint().RequireAuthorization("ApiKeyPolicy");
                 });
