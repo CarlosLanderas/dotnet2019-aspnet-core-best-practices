4290de9a58dc46a7d194a3e336511d3dc0c7c840
Carlos Landeras <clanderas@plainconcepts.com>
2019-06-16 13:09:03 +0200

    Add Span based file parsing to serve over BodyWriter

--- src/DotNet2019.Api/Infrastructure/Middleware/SecretMiddleware.cs
+++ src/DotNet2019.Api/Infrastructure/Middleware/SecretMiddleware.cs
@@ -2,7 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Text;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DotNet2019.Api.Infrastructure.Middleware
@@ -10,15 +11,33 @@ namespace DotNet2019.Api.Infrastructure.Middleware
     public class SecretMiddleware : IMiddleware
     {
         private readonly DiagnosticListener _diagnosticListener;
-
+        private ReadOnlyMemory<byte> fileContent;
+        
         public SecretMiddleware(DiagnosticListener diagnosticListener)
         {
             _diagnosticListener = diagnosticListener;
+            fileContent = File.ReadAllBytes("DataContent.txt");
         }
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
-        {
-            var content = Encoding.UTF8.GetBytes("Hello from the secret endpoint");
-            await context.Response.BodyWriter.WriteAsync(content);
+        {  
+   
+            var bytesConsumed = 0;
+            var linePosition = 0;
+
+            do
+            {
+                linePosition = fileContent.Slice(bytesConsumed).Span.IndexOf((byte)'\n');
+
+                if (linePosition >= 0)
+                {                    
+                    var lineLength = (bytesConsumed + linePosition) - bytesConsumed;
+
+                    await context.Response.BodyWriter.WriteAsync(fileContent.Slice(bytesConsumed, lineLength));
+
+                    bytesConsumed += lineLength + 1;
+                }
+            }
+            while (linePosition >= 0);
         }
     }
 }
