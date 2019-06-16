1082910c627957c8da2c298e5d8cbfab86833aae
lurumad <luis_ruiz_pavon@hotmail.com>
2019-06-16 09:04:34 +0200

    Add problem details

--- src/DotNet2019.Api/Configuration.cs
+++ src/DotNet2019.Api/Configuration.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DotNet2019.Api
 {
@@ -12,21 +17,40 @@ namespace DotNet2019.Api
         {
             return services
                 .AddMvc()
+                .AddApplicationPart(typeof(Configuration).Assembly)
                 .Services;
         }
 
         public static IApplicationBuilder Configure(
             IApplicationBuilder app,
+            IWebHostEnvironment environment,
             Func<IApplicationBuilder, IApplicationBuilder> configureHost)
         {
             return configureHost(app)
+                .UseProblemDetails(configure =>
+                {
+                    configure.IncludeExceptionDetails = _ => environment.IsDevelopment();
+                })
+                .Use(CustomMiddleware)
                 .UseEndpoints(endpoints =>
                 {
                     endpoints.MapControllerRoute(
                         name: "default",
                         pattern: "{controller=Home}/{action=Index}/{id?}");
-                    endpoints.MapRazorPages();
                 });
+        }
+
+        private static Task CustomMiddleware(HttpContext context, Func<Task> next)
+        {
+            if (context.Request.Path.StartsWithSegments("/middleware", out _, out var remaining))
+            {
+                if (remaining.StartsWithSegments("/error"))
+                {
+                    throw new Exception("This is an exception thrown from middleware.");
+                }
+            }
+
+            return next();
         }
     }
 }
--- src/DotNet2019.Host/Startup.cs
+++ src/DotNet2019.Host/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNet2019.Api;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,19 +15,24 @@ namespace DotNet2019.Host
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.ConfigureServices(services);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app
-            .UseRouting()
-            .UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            Configuration.Configure(app, env, host =>
+                host
+                    .UseRouting()
+                    .UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapGet("/", async context =>
+                        {
+                            await context.Response.WriteAsync("Hello DotNet 2019!");
+                        });
+                    })
+            );
+
+            
         }
     }
 }
